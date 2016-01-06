f90c9f6fae71737f506c194872533b352963c215
Lorena Bauleo <lore.bauleo@gmail.com>
2016-01-06 09:20:58 -0300

    Mejoras realizadas
    -Se definio la clase como estatica
    -Se quito el constructor.
    -Los flags se declararon como public properties
    -Se removio el field "_Initialized" por no ser necesario
    -Se cambio el parametro "message" por resultar ambiguo
    -Se utilizo el metodo String.IsNullOrWhiteSpace para validar el mensaje
    -Se agrego Excepcion cuando el mensaje esta vacio
    -Se crearon metodos independientes para loguear los distintos niveles de
    mensaje
    -Se declaro como privado el metodo que gestiona el logueo y se renombro
    -Se agregaron validaciones para los distintos medios de salvado
    -Se agrego control de excepciones para las operaciones de I/O (DDBB &
    File)
    -Se utilizo using para el control de objetos de Connection y Command
    -Se asigno la Connection al Command
    -Se reemplazo el uso de ReadAllText y WriteAllText por AppendAllText
    -Se corrigio la asignacion de la variable "l" para que muestre el nivel
    de log
    -Se inicializaron las variables "t" y "l" para controlar si existen
    cambios para loguear o no.
    -Se resetea color de la consola luego de loggear por si se utiliza la
    misma fuera de la clase.

---
+++ ExerciseBelatrix/JobLogger.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ExerciseBelatrix
+{
+    public static class JobLogger
+    {
+        public static bool LogToDatabase { get; set; }
+        public static bool LogToFile { get; set; }
+        public static bool LogToConsole { get; set; }
+        public static bool LogMessages { get; set; }
+        public static bool LogWarnings { get; set; }
+        public static bool LogErrors { get; set; }
+
+        public static void LogMessage(string message)
+        {
+            Log(message, true, false, false);
+        }
+
+        public static void LogWarning(string message)
+        {
+            Log(message, false, true, false);
+        }
+
+        public static void LogError(string message)
+        {
+            Log(message, false, false, true);
+        }
+
+        private static void Log(string msg, bool message, bool warning, bool error)
+        {
+
+            if (String.IsNullOrWhiteSpace(msg))
+            {
+                throw new Exception("Null or Empty Message");
+            }
+
+            if (!LogToConsole && !LogToFile && !LogToDatabase)
+            {
+                throw new Exception("Invalid configuration");
+            }
+            if ((!LogErrors && !LogMessages && !LogWarnings) || (!message && !warning && !error))
+            {
+                throw new Exception("Error or Warning or Message must be specified");
+            }
+
+            if (LogToDatabase)
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.AppSettings["ConnectionString"]))
+                {
+                    int t = 0;
+                    if (message && LogMessages)
+                    {
+                        t = 1;
+                    }
+                    if (error && LogErrors)
+                    {
+                        t = 2;
+                    }
+                    if (warning && LogWarnings)
+                    {
+                        t = 3;
+                    }
+
+                    if (t > 0)
+                    {
+                        using (SqlCommand command = new SqlCommand("Insert into Log Values('" + message + "', " + t.ToString() + ")", connection))
+                        {
+                            try
+                            {
+                                connection.Open();
+                                command.ExecuteNonQuery();
+                            }
+                            catch (Exception ex)
+                            {
+                                connection.Close();
+                                throw new Exception("Error logging to database");
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            if (LogToFile)
+            {
+                string l = null;
+
+
+                if (error && LogErrors)
+                {
+                    l = "Err - " + DateTime.Now.ToShortDateString() + " - " + message;
+                }
+                if (warning && LogWarnings)
+                {
+                    l = "Wrn - " + DateTime.Now.ToShortDateString() + " - " + message;
+                }
+                if (message && LogMessages)
+                {
+                    l = "Msg - " + DateTime.Now.ToShortDateString() + " - " + message;
+                }
+
+                if (!String.IsNullOrEmpty(l))
+                {
+                    try
+                    {
+                        File.AppendAllText(ConfigurationManager.AppSettings["LogFileDirectory"] + "LogFile" + DateTime.Now.ToShortDateString() + ".txt", l);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("Error logging to file");
+                    }
+
+                }
+            }
+
+            if (LogToConsole)
+            {
+                if (error && LogErrors)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                if (warning && LogWarnings)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                if (message && LogMessages)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine(DateTime.Now.ToShortDateString() + message);
+
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ ExerciseBelatrixUnitTests/BelatrixExerciseTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExerciseBelatrix;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ExerciseBelatrixUnitTests
+{
+    [TestClass]
+    public class BelatrixExerciseTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void EmptyMessageTest()
+        {
+            JobLogger.LogMessage("   ");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void InvalidConfigurationTest()
+        {
+            JobLogger.LogMessage("Log");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void FlagConfigurationTest()
+        {
+            JobLogger.LogToConsole = true;
+            JobLogger.LogMessage("Log");
+        }
+
+        [TestMethod]
+        public void ValidConfigurationTest()
+        {
+            JobLogger.LogToConsole = true;
+            JobLogger.LogMessages = true;
+
+            JobLogger.LogMessage("Log");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void LogToDataBaseTest()
+        {
+            JobLogger.LogToDatabase = true;
+            JobLogger.LogMessages = true;
+
+            JobLogger.LogMessage("Este es mi mensaje");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void LogToFileTest()
+        {
+            JobLogger.LogToFile = true;
+            JobLogger.LogMessages = true;
+
+            JobLogger.LogMessage("Este es mi mensaje");
+        }
+    }
+}
